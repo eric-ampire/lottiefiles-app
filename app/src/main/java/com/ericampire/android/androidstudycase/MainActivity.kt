150bc30d7e89bbf2cad869bc5548e34d8722aa30
Eric Ampire <ericampire.top@gmail.com>
2021-08-26 06:20:01 +0200

    Update gradle plugin

--- app/src/main/java/com/ericampire/android/androidstudycase/MainActivity.kt
+++ app/src/main/java/com/ericampire/android/androidstudycase/MainActivity.kt
@@ -11,28 +11,28 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.ericampire.android.androidstudycase.ui.theme.AndroidStudyCaseTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            AndroidStudyCaseTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      AndroidStudyCaseTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(color = MaterialTheme.colors.background) {
+          Greeting("Android")
         }
+      }
     }
+  }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+  Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    AndroidStudyCaseTheme {
-        Greeting("Android")
-    }
+  AndroidStudyCaseTheme {
+    Greeting("Android")
+  }
 }
