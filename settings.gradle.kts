e9efc3f2ab72b99d332a5fb1231c1ad4ca81681e
Eric Ampire <ericampire.top@gmail.com>
2021-09-03 15:01:49 +0200

    Update app name and module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,14 +7,14 @@ refreshVersions {
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
   repositories {
     google()
     mavenCentral()
     jcenter() // Warning: this repository is going to shut down soon
   }
 }
-rootProject.name = "android-study-case"
+rootProject.name = "lottiefiles"
 include(":app")
 include(":util")
 include(":i18n")
