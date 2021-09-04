0b64eb52a60d98acc2e8ca31138ff3ccb171c158
Eric Ampire <ericampire.top@gmail.com>
2021-09-04 03:29:24 +0200

    Adding End-point

---
+++ util/src/main/java/com/ericampire/android/androidstudycase/util/ApiUrl.kt
@@ -0,0 +1,17 @@
+package com.ericampire.android.androidstudycase.util
+
+object ApiUrl {
+  object LottieFile {
+    const val featured = "https://firebasestorage.googleapis.com/v0/b/lottiefiles-test.appspot.com/o/featuredAnimations.json?alt=media&token=f6e406f5-befb-40ab-a9b0-bb0a773b09fd"
+    const val recent = "https://firebasestorage.googleapis.com/v0/b/lottiefiles-test.appspot.com/o/recentAnimations.json?alt=media&token=f5acfd96-384a-4552-a0b5-399675a03826"
+    const val popular = "https://firebasestorage.googleapis.com/v0/b/lottiefiles-test.appspot.com/o/popularAnimations.json?alt=media&token=a32b4948-d278-4923-880e-8fb57741c190"
+  }
+
+  object Blog {
+    const val latest = "https://firebasestorage.googleapis.com/v0/b/lottiefiles-test.appspot.com/o/blogs.json?alt=media&token=c6bf2153-7a69-4a47-9e3a-6f7500d8f523"
+  }
+
+  object Animator {
+    const val featured = "https://firebasestorage.googleapis.com/v0/b/lottiefiles-test.appspot.com/o/featuredAnimators.json?alt=media&token=5b3e8205-b317-45c4-a5ce-36c9dc57911d"
+  }
+}
